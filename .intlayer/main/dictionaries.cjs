0e58a15e1b9fa819ac292dea2284eff1d2cb8268
Fadhel <fadhel@tutamail.com>
2026-04-14 01:43:46 +0300

    chore: remove .intlayer from tracking

--- .intlayer/main/dictionaries.cjs
+++
@@ -1,9 +0,0 @@
-const _CxWmGzEj0P7YZZdQIJ9g = require('../dictionary/page.json');
-
-const dictionaries = {
-  "page": _CxWmGzEj0P7YZZdQIJ9g
-};
-const getDictionaries = () => dictionaries;
-
-module.exports.getDictionaries = getDictionaries;
-module.exports = dictionaries;
--- .intlayer/main/dictionaries.mjs
+++
@@ -1,9 +0,0 @@
-import _CxWmGzEj0P7YZZdQIJ9g from '../dictionary/page.json' with { type: 'json' };
-
-const dictionaries = {
-  "page": _CxWmGzEj0P7YZZdQIJ9g
-};
-const getDictionaries = () => dictionaries;
-
-export { getDictionaries };
-export default dictionaries;
