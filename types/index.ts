ca9a88a3cd9a71670e0064b5306a5f4a22956823
Fadhel <fadhel@tutamail.com>
2026-04-24 21:25:32 +0300

    more imports tweaks

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
-import { Decimal } from "@/lib/generated/prisma/internal/prismaNamespace";
-import { insertProductSchema, ProductResponseSchema, signInFormSchema ,  } from "@/lib/validators";
+
+import { ProductResponseSchema, signInFormSchema ,  } from "@/lib/validators";
 import z from "zod";
 
 export type Product = z.infer<typeof ProductResponseSchema> 
